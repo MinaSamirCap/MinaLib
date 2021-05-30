da9a5e85c0123c1aa8b48325906327e324179c6e
MinaSamir <menasamercap@gmail.com>
2021-05-30 12:44:43 +0200

    fix ..

--- app/src/main/java/com/mmd/minalib/MainActivity.kt
+++ app/src/main/java/com/mmd/minalib/MainActivity.kt
@@ -40,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         /// a dummy comment ..
         /// a dummy comment ..
         /// a dummy comment ..
+        /// a dummy comment ..
 
         /// another dummy comment ..
         /// another dummy comment ..
