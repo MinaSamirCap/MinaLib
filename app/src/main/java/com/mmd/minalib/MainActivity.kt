e809f30c2ed2e7a1b0d905d065ad85b39ea89b34
MinaSamir <menasamercap@gmail.com>
2021-07-09 17:32:41 +0200

    adde a dummy commit

--- app/src/main/java/com/mmd/minalib/MainActivity.kt
+++ app/src/main/java/com/mmd/minalib/MainActivity.kt
@@ -46,6 +46,7 @@ class MainActivity : AppCompatActivity() {
         /// a dummy comment ..
         /// a dummy comment ..
         /// a dummy comment ..
+        /// a dummy comment ..
 
         /// another dummy comment ..
         /// another dummy comment ..
