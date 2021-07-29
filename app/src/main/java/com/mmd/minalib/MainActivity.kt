dd24ad5b31d274a25285b5ea6587ba5f3e27c78e
MinaSamir <menasamercap@gmail.com>
2021-07-29 12:51:57 +0200

    a dummy commit

--- app/src/main/java/com/mmd/minalib/MainActivity.kt
+++ app/src/main/java/com/mmd/minalib/MainActivity.kt
@@ -51,6 +51,7 @@ class MainActivity : AppCompatActivity() {
         /// a dummy comment ..
         /// a dummy comment ..
         /// a dummy comment ..
+        /// a dummy comment ..
 
         /// another dummy comment ..
         /// another dummy comment ..
