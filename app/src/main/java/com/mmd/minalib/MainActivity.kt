5ce3497fe4790f51bede2e68607fa44aeaf27e5d
Mina Samir <MenaSamerCap@gmail.com>
2021-12-19 00:13:33 +0200

    a dummy commit

--- app/src/main/java/com/mmd/minalib/MainActivity.kt
+++ app/src/main/java/com/mmd/minalib/MainActivity.kt
@@ -75,6 +75,7 @@ class MainActivity : AppCompatActivity() {
         /// a dummy comment ..
         /// a dummy comment ..
         /// a dummy comment ..
+        /// a dummy comment ..
 
         /// another dummy comment ..
         /// another dummy comment ..
