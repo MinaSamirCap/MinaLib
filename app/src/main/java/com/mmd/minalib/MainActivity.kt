ad646d04a1b4e73a9aeb7a5a24392beeca8aebc4
Mina Samir <MenaSamerCap@gmail.com>
2022-11-04 14:25:23 +0200

    anotehr dummy commit

--- app/src/main/java/com/mmd/minalib/MainActivity.kt
+++ app/src/main/java/com/mmd/minalib/MainActivity.kt
@@ -247,6 +247,7 @@ class MainActivity : AppCompatActivity() {
         /// another dummy comment ..
         /// another dummy comment ..
         /// another dummy comment ..
+        /// another dummy comment ..
 
         findViewById<TextView>(R.id.tv).setOnClickListener {
             MinaLibrary.minaToast(this)
